021ccb73fe15e763bf157e56c41656f78fabca9d
Ankit Choudhary अंकित चौधरी <ac2000choudhary@gmail.com>
2025-07-14 23:58:21 +0530

    feat: update project name and enhance package dependencies
    - Changed project name from "holicraft-storefront" to "snehkunj-storefront" in package-lock.json and package.json.
    - Added new dependency "tsx" to facilitate TypeScript execution in scripts.
    - Updated environment variable handling in sanity/env.ts for improved configuration management.
    - Refactored CSS variables in globals.css for better design consistency.
    - Enhanced seed-dummy-data.ts script with improved logging and error handling for data seeding process.

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,15 +1,26 @@
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-03-13'
+import dotenv from "dotenv";
 
-export const dataset = process.env.SANITY_STUDIO_DATASET || 'production'
+dotenv.config({ path: ".env.local" });
 
-export const projectId = process.env.SANITY_STUDIO_PROJECT_ID || 'kiiaxm1k'
+export const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-03-13";
 
-export const useCdn = process.env.NODE_ENV === 'production'
+export const dataset =
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET ||
+  process.env.SANITY_STUDIO_DATASET ||
+  "production";
+
+export const projectId =
+  process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID ||
+  process.env.SANITY_STUDIO_PROJECT_ID ||
+  "kiiaxm1k";
+
+export const useCdn = process.env.NODE_ENV === "production";
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
-    throw new Error(errorMessage)
+    throw new Error(errorMessage);
   }
 
-  return v
+  return v;
 }
